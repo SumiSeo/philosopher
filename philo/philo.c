4ed3012edb1580b566d0f8f8eb2b533f41eb9506
Sumi <qkobr94@gmail.com>
2024-08-15 16:17:27 +0900

    feat/start-parsing

--- philo/philo.c
+++ philo/philo.c
@@ -1,14 +1,32 @@
 #include "philo.h"
 
-int main(int argc, char **argv){
-  
-    t_philos  *philos;
-    if(argc != 5 && argc!=6)
+void init_philos_info(t_philos *philos, char **argv)
+{
+    (void)philos;
+    (void)argv;
+}
+
+int main(int argc, char **argv)
+{
+
+    t_philos *philos;
+    int errno;
+    philos = malloc(sizeof(t_philos));
+    if (!philos) {
+        print_error("Memory allocation for philo-argv failed");
+        return 1; 
+    }
+
+    if (argc != 5 && argc != 6)
         print_error("error argc");
     else
+    {
         printf("fiine for now");
-    (void)philos;
-    int errno;
-    (void)argv;
+        init_philos_info(philos, argv);
+         
+    }
+   
+
+    free(philos);
     return 0;
 }
